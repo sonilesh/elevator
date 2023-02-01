6131b08f99a33f938f6ddf4c68f0297025397960
Nilesh Soni <soninilesh0809@gmail.com>
2023-02-01 15:29:37 +0530

    added simple get route api

--- elevator_problem/basic/views.py
+++ elevator_problem/basic/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 # Create your views here.
+
+
+@api_view(['GET'])
+def get_routes(request):
+    '''
+    api for displaying the available endpoints with body, method, description
+    '''
+    routes = [
+        {
+            "endpoint": "/elevator/",
+            "method": "POST",
+            "body": {
+                "count" : "n",
+
+            },
+            "description": "Create n new elevators"
+        },
+    ]
+    return Response(routes)
+
