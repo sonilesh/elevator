8502a4d9c6202eed2e5fc0a5bbd1173cc6feed14
Nilesh Soni <soninilesh0809@gmail.com>
2023-02-01 12:54:06 +0530

    Setup postgres database for the project

--- elevator_problem/elevator_problem/settings.py
+++ elevator_problem/elevator_problem/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'elevator_problem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
