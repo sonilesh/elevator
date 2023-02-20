6c986f139d7e3ac3a15b043cdc4fe0bedaf0b63d
Nilesh Soni <soninilesh0809@gmail.com>
2023-02-20 16:55:10 +0530

    added helper fle

---
+++ elevator_problem/basic/helpers.py
@@ -0,0 +1,21 @@
+START = 'start'
+STOP = 'stop'
+UP = 'up'
+DOWN = 'down'
+CLOSE = 'close'
+OPEN = 'open'
+
+RUNNING_STATUS_CHOICES = [
+    (START, 'Start'),
+    (STOP, 'Stop'),
+]
+
+DOOR_STATUS_CHOICES = [
+    (OPEN, 'Open'),
+    (CLOSE, 'Close'),
+]
+
+DIRECTION_CHOICES = [
+    (UP, 'Up'),
+    (DOWN, 'Down'),
+]
