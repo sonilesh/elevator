24db0cba16c85247728d9e3398c2c4bc9179a5a3
Nilesh Soni <soninilesh0809@gmail.com>
2023-02-22 12:35:58 +0530

    made changes and added apis

--- elevator_problem/basic/models.py
+++ elevator_problem/basic/models.py
@@ -5,18 +5,25 @@ from .helpers import *
 
 class Elevator(models.Model):
     current_floor = models.IntegerField(default = 0)
-    next_floor = models.IntegerField()
     running_status = models.CharField(max_length = 5, choices = RUNNING_STATUS_CHOICES, default = STOP)
     operational = models.BooleanField(default = True)
     door_status = models.CharField(max_length = 5, choices = DOOR_STATUS_CHOICES, default = CLOSE)
-    direction = models.CharField(max_length = 4, choices = DIRECTION_CHOICES, default = UP)
+    initial_direction = models.CharField(max_length = 4, choices = DIRECTION_CHOICES, default = UP)
+    lift_requested_direction = models.CharField(max_length = 4, choices = DIRECTION_CHOICES, default = UP)
+    next_floor = models.IntegerField(default = -1)
 
 
 class UserRequest(models.Model):
     elevator = models.ForeignKey('Elevator', on_delete = models.CASCADE)
     requested_floor = models.IntegerField(null = False)
+    created = models.DateTimeField(auto_now_add = True)
+    requested_direction = models.CharField(max_length = 4, choices = DIRECTION_CHOICES, default = None)
+    completed = models.BooleanField(default = False)
 
     def __str__(self):
         return "Requested " + self.elevator.id + " for " + self.requested_floor
+    
+    class Meta:
+        ordering = ['-created']
 
 
--- elevator_problem/basic/views.py
+++ elevator_problem/basic/views.py
@@ -5,7 +5,15 @@ from rest_framework.response import Response
 from . import models
 from . import serializers
 from .helpers import *
-# Create your views here.
+import threading
+import time
+
+
+### Assumptions :
+
+# 1. lift takes 1 sec to move 1 floor (up/down)
+# 2. door open/close takes 3 secs
+# 3. door will automatically closed after 5 secs if doors are already opened.
 
 
 @api_view(['GET'])
@@ -51,24 +59,39 @@ def create_elevators(request):
 @api_view(['PUT'])
 def open_door(request, pk):
     elevator = get_object_or_404(models.Elevator, id = pk)
-    data = {"door_status": OPEN}
-    serializer = serializers.ElevatorSerializer(elevator, data = data, partial = True)
-    if serializer.is_valid():
-        serializer.save()
+    if elevator['running_status'] == STOP and elevator['door_status'] == CLOSE:              ### add condition for checking if lift is moving or not
+        data = {"door_status": OPEN}
+        serializer = serializers.ElevatorSerializer(elevator, data = data, partial = True)
+        if serializer.is_valid():
+            print("Opening the lift!!!")
+            time.sleep(3)                                                                    ### added time of 3 secs to open lift
+            serializer.save()
+            close_thread = threading.Timer(5, close_door_helper(elevator))                   ### after 5 secs doors will automatically closed
+            close_thread.start()
+        else:
+            return Response("Invalid data found", status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
     else:
-        return Response("Invalid data found", status.HTTP_400_BAD_REQUEST)
-    return Response(serializer.data)
+        return Response("Lift is moving hence not able to open the door!!")
+
+def close_door_helper(elevator):
+    if elevator['running_status'] == STOP and elevator['door_status'] == OPEN:                 ### condition to check if lift is not moving and doors are open
+        data = {"door_status": CLOSE}
+        serializer = serializers.ElevatorSerializer(elevator, data = data, partial = True)
+        if serializer.is_valid():
+            print("Closing the lift!!")
+            time.sleep(3)                                                                       ### added time of 3 secs to close the lift
+            serializer.save()
+        else:
+            return Response("Invalid data found", status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
+    else:
+        return Response("Lift is moving hence doors are already closed!!")
 
 @api_view(['PUT'])
 def close_door(request, pk):
     elevator = get_object_or_404(models.Elevator, id = pk)
-    data = {"door_status": CLOSE}
-    serializer = serializers.ElevatorSerializer(elevator, data = data, partial = True)
-    if serializer.is_valid():
-        serializer.save()
-    else:
-        return Response("Invalid data found", status.HTTP_400_BAD_REQUEST)
-    return Response(serializer.data)
+    return close_door_helper(elevator)
 
 @api_view(['PUT'])
 def mark_lift_not_operational(request, pk):
@@ -81,13 +104,56 @@ def mark_lift_not_operational(request, pk):
         return Response("Invalid data found", status.HTTP_400_BAD_REQUEST)
     return Response(serializer.data)
 
+@api_view(['GET'])
+def next_floor(request, pk):
+    req = models.Elevator.objects.get(elevator = pk)
+    if req:
+        return req["next_floor"]
+
+    return Response("There is no lift with this id!!!")
+
+@api_view(['GET'])
+def fetch_all_request_of_lift(request, pk):
+    req = models.UserRequest.objects.get(elevator = pk)
+    if not len(req):
+        return Response("There is no request made hence next floor can't be calculated")
+    serializer = UserRequestSerializer(req, many=True)
+    return Response(serializer.data)
+ 
+@api_view(['GET'])
+def fetch_direction(request, pk):
+    req = models.Elevator.objects.get(elevator = pk)
+    if req:
+        return req["lift_requested_direction"] if req["lift_requested_direction"] else req["initial_direction"]
+
+    return Response("There is no lift with this id!!!")
+
+
 @api_view(['POST'])
 def make_request(request, pk):
+    # 1. Add request to the lift
+    # 2. Update the next destination of the lift
+    # 3. Keep checking the requests for next destination after every seconds
     elevator = get_object_or_404(models.Elevator, id = pk)
     data = request.data
     user_request = models.UserRequest.objects.create(
         elevator = elevator,
-        requested_floor = data["floor"],
+        requested_floor = data["floor"]
     )
     serializer = serializers.UserRequestSerializer(user_request, many=False)
+    process_request_thread = threading.Thread(process_request)
+    process_request_thread.start()
     return Response(serializer.data)
+
+@api_view(['POST'])
+def call_lift(request):
+    # 1. find out which lift needs to be called/assigned to user
+    # 2. Add request to the lift
+    # 3. Update the next destination of the lift
+    # 4. Keep checking the requests for next destination after every seconds
+    pass
+
+def process_request():
+    # sort the requests and move lift by one floor and check all request again if request comes with same direction or not.
+    pass
+
